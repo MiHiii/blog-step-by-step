6e1fe4bfb757585460a5dc0eb4a498c093ea3d87
MiHiii <hieupham08111@gmail.com>
2024-03-09 08:33:37 +0700

    connect to the database p2

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+//Để có thể kết nối với môi trường
+dotenv.config();
+
+//Kết nối cơ sở dữ liệu
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
