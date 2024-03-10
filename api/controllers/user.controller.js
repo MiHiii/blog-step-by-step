7223dfa55c3e6efe4313e0a478e15807ab87820e
MiHiii <hieupham08111@gmail.com>
2024-03-10 09:44:40 +0700

    create test api routes

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+//Đây là named export
+
+export const test = (req, res) => {
+  res.json({ message: "API is working!" });
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/user.route.js";
+
 //Để có thể kết nối với môi trường
 dotenv.config();
 
@@ -20,3 +22,11 @@ const app = express();
 app.listen(3000, () => {
   console.log("Sever is running on port 3000!");
 });
+
+//Nếu không có file api riêng thì sẽ viết thế này
+// app.get("/test", (req, res) => {
+//   res.json({ message: "API is working" });
+// });
+
+//Có api riêng routes thay "get" => "use"
+app.use("/api/user", userRoutes);
