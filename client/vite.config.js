29ca52e5de4fa3fdc73768442fb36bcac770020d
MiHiii <hieupham08111@gmail.com>
2024-03-11 17:47:15 +0700

    complete sign in page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //Chuyển đổi proxy khi người dùng SIGNUP
   server: {
     proxy: {
       "/api": {
