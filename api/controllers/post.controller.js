8fad21c9e4243936f8c937dc5d03c0e337d44113
MiHiii <hieupham08111@gmail.com>
2024-04-21 14:58:04 +0700

    add  update post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -86,3 +86,28 @@ export const deletepost = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updatepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to update this post!'));
+  }
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.postId,
+      {
+        $set: {
+          title: req.body.title,
+          category: req.body.category,
+          image: req.body.image,
+          content: req.body.content,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
+};
