1ec321a8a92ad96b17da3db5642ee674ce6babe4
MiHiii <hieupham08111@gmail.com>
2024-04-27 18:08:02 +0700

    add recent article section to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,15 +1,16 @@
+import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Spinner, Button } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection';
+import PostCard from '../components/PostCard';
 
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -34,6 +35,22 @@ export default function PostPage() {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+
   if (loading)
     return (
       <div className='flex justify-center items-center min-h-screen'>
@@ -41,26 +58,25 @@ export default function PostPage() {
       </div>
     );
   return (
-    <main className='flex flex-col p-3 max-w-6xl mx-auto min-h-screen '>
+    <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
       <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
         {post && post.title}
       </h1>
       <Link
         to={`/search?category=${post && post.category}`}
-        className='mt-5 self-center'
+        className='self-center mt-5'
       >
-        <Button gradientDuoTone={'tealToLime'} outline size='xs' pill>
+        <Button color='gray' pill size='xs'>
           {post && post.category}
         </Button>
       </Link>
       <img
         src={post && post.image}
         alt={post && post.title}
-        className='mt-10 mb-3 max-h-[600px] w-full object-cover rounded shadow-lg'
+        className='mt-10 p-3 max-h-[600px] w-full object-cover'
       />
       <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
-        <span>{post && new Date(post.updatedAt).toLocaleDateString()}</span>
-        {/* Hiện số phút đọc */}
+        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
         <span className='italic'>
           {post && (post.content.length / 1000).toFixed(0)} mins read
         </span>
@@ -69,10 +85,18 @@ export default function PostPage() {
         className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
-      <div className='max-w-4xl mx-auto w-full '>
+      <div className='max-w-4xl mx-auto w-full'>
         <CallToAction />
       </div>
       <CommentSection postId={post._id} />
+
+      <div className='flex flex-col justify-center items-center mb-5'>
+        <h1 className='text-xl mt-5'>Recent articles</h1>
+        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
+      </div>
     </main>
   );
 }
